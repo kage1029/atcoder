6d6ec591acdbdd22d296fbb6137e5842f59acdc4
kage1029 <ichika789rider@gmail.com>
2020-04-07 03:28:51 +0900

    practice

--- atcoder143D.rb
+++ atcoder143D.rb
@@ -1,17 +1,17 @@
 n = gets.chomp.to_i
 ary = gets.split(" ").map(&:to_i).sort.reverse
-
-p ary
 ans = 0
 
 (n-2).times do |i|
-	a = ary.bsearch_index{|x| ary[i] >= ary[i+1] + x}
-	puts a
-	if a != nil
-		ans += ary[0..a].size
-	else
-		ans += ary[0..-1].size
-		puts "ok"
+	j = i + 1
+	k = n - 1
+	while j < k
+		if ary[i] < ary[j] + ary[k]
+			ans += k - j
+			j += 1
+		else
+			k -= 1
+		end
 	end
 end
 
